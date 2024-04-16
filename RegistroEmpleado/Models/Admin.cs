85400547ceb3b4eeb1b5bf983031c0a904843104
AnthonyCarmine <anthonymunoz302755@correo.itm.edu.co>
2024-04-15 20:23:14 -0500

    Se Crea la base de datos y los Models

---
+++ RegistroEmpleado/Models/Admin.cs
@@ -0,0 +1,17 @@
+namespace RegistroEmpleado.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public int Cedula { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Names { get; set; }
+        public string LastNames { get; set; }
+        public string PhotoProfile { get; set; }
+        public string Password { get; set; }
+        public DateTime LoginAt { get; set; }
+        public DateTime LogOffAt { get; set; }
+
+    }
+}
---
+++ base.sql
@@ -0,0 +1,27 @@
+-- Active: 1713229490129@@b18vxxgzlvikzwupgdyo-mysql.services.clever-cloud.com@3306@b18vxxgzlvikzwupgdyo
+CREATE TABLE Admins (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Cedula INT UNIQUE NOT NULL,
+    Email VARCHAR(255) UNIQUE NOT NULL,
+    Phone VARCHAR(20) NOT NULL,
+    Names VARCHAR(45) NOT NULL,
+    LastNames VARCHAR(45) NOT NULL,
+    PhotoProfile VARCHAR(255) NOT NULL,
+    Password VARCHAR
+    (45) NOT NULL,
+    LoginAt DATE,
+    LogOffAt DATE
+);
+
+CREATE TABLE Empleoyees (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Cedula INT UNIQUE NOT NULL,
+    Email VARCHAR(255) UNIQUE NOT NULL,
+    Phone VARCHAR(20) NOT NULL,
+    Names VARCHAR(45) NOT NULL,
+    LastNames VARCHAR(45) NOT NULL,
+    PhotoProfile VARCHAR(255) NOT NULL,
+    Password VARCHAR(45) NOT NULL,
+    LoginAt DATE,
+    LogOffAt DATE
+);
