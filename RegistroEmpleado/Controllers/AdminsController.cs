68fa2a2ad02d435ecd9917666dfae3eedfc5fba3
AnthonyCarmine <anthonymunoz302755@correo.itm.edu.co>
2024-04-16 15:01:02 -0500

    Se crea controlador para ver la vista

--- RegistroEmpleado/Controllers/AdminsController.cs
+++ RegistroEmpleado/Controllers/AdminsController.cs
@@ -1,10 +1,21 @@
-
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using RegistroEmpleado.Models;
+using RegistroEmpleado.Data;
 
 namespace RegistroEmpleado.Controllers
 {
     public class  AdminsController : Controller
     {
-        
+        public readonly BaseContext _context;
+
+        public AdminsController(BaseContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Admins.ToListAsync());
+        }
     }
 }
