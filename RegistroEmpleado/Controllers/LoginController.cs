561de47a58f69b292dfddeb23e34fef05be4f1fc
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-04-16 01:12:23 -0500

    Metodo de login

--- RegistroEmpleado/Controllers/LoginController.cs
+++ RegistroEmpleado/Controllers/LoginController.cs
@@ -1,12 +1,31 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
+using RegistroEmpleado.Data;
 
 namespace RegistroEmpleado.Controllers;
 
 public class LoginController : Controller
 {
+    private readonly BaseContext _context;
+    
+    public LoginController(BaseContext context)
+    {
+        _context = context;
+    }
     public IActionResult Index()
     {
         return View();
+    }
+
+    public IActionResult Acceder(string username, string password)
+    {
+        var user = _context.Admins.AsQueryable();
+        
+        if (user.Any(u => u.Names == username && u.Password == password))
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View("Index", "Login");
     }
 }
